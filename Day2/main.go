a9cbd93902a4f8d2f22cb7f713eaea46e4edbecb
Heshan Padmasiri <hpheshan@gmail.com>
2024-12-07 20:42:46 +0530

    Day 2 part 2 solved

--- Day2/main.go
+++ Day2/main.go
@@ -26,39 +26,107 @@ func main() {
 	sum := 0
 	for _, report := range input.reports {
 		if isSafe(report) {
+			printReport(report)
 			sum++
 		}
 	}
 	fmt.Println(sum)
 }
 
+func printReport(report Report) {
+	for i, val := range report.vals {
+		if i > 0 {
+			fmt.Print(" ")
+		}
+		fmt.Print(val)
+	}
+	fmt.Println()
+}
+
 func isSafe(report Report) bool {
-	if len(report.vals) == 0 {
+	vals := report.vals
+	if isSafeInner(vals) {
+		return true;
+	}
+	for i := 0; i < len(report.vals); i++ {
+		newVals := skipIndex(vals, i)
+		if isSafeInner(newVals) {
+			isSafeInner(newVals)
+			return true
+		}
+	}
+	return false;
+}
+
+func skipIndex(vals []int, index int) []int {
+	newVals := make([]int, 0)
+	for i, val := range vals {
+		if i != index {
+			newVals = append(newVals, val)
+		}
+	}
+	return newVals
+}
+
+func isIncreasing(vals []int) bool {
+	if len(vals) < 2 {
 		return true
 	}
-	var increasing bool
-	var diff = report.vals[0] - report.vals[1]
-	if diff == 0 || abs(diff) > 3 {
-		return false
-	}
-	if diff > 0 {
-		increasing = false
-	} else {
-		increasing = true
-	}
-	for i := 2; i < len(report.vals); i++ {
-		diff = report.vals[i-1] - report.vals[i]
-		if diff == 0 || abs(diff) > 3 {
-			return false
-		}
-		if increasing && diff > 0 {
-			return false
-		}
-		if !increasing && diff < 0 {
+	for i := 0; i < len(vals) - 1; i++ {
+		if vals[i] >= vals[i + 1] || vals[i + 1] - vals[i] > 3 {
 			return false
 		}
 	}
 	return true
+}
+
+func isDecreasing(vals []int) bool {
+	if len(vals) < 2 {
+		return true
+	}
+	for i := 0; i < len(vals) - 1; i++ {
+		if vals[i] <= vals[i + 1] || vals[i] - vals[i + 1] > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func isMonotonic(vals []int) bool {
+	if len(vals) == 0 {
+		return false
+	}
+	if len(vals) == 1 {
+		return true
+	}
+	diff := vals[0] - vals[1]
+	if diff == 0 {
+		return false
+	}
+	for i := 2; i < len(vals); i++ {
+		newDiff := vals[i-1] - vals[i]
+		if newDiff == 0 {
+			return false
+		}
+		if diff > 0 && newDiff < 0 || diff < 0 && newDiff > 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func isSafeDiff(vals []int) bool {
+	for i := 0; i < len(vals) - 1; i++ {
+		diff := vals[i] - vals[i+1]
+		if abs(diff) > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func isSafeInner(vals []int) bool {
+	return isIncreasing(vals) || isDecreasing(vals);
 }
 
 func abs(x int) int {
